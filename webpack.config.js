969895e1489cfd38419d480c29f7f4214feb52a8
ahmet kutlu sahin <kutlugsahin@gmail.com>
2018-03-28 21:56:20 +0200

    library name

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'), // eslint-disable-line
 		filename: 'index.js',
-		library: 'ReactSmoothDND',
+		library: 'ReactSmoothDnD',
 		libraryTarget: "umd",
 	},
 	module: {
