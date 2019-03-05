456fda3c5bc69c9b2a68131879bd977f2ec77cc5
Kutlu Sahin <ksahin@sdl.com>
2019-03-05 14:13:24 +0100

    build process

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = function(api) {
+	api.cache(true);
+	return {
+		presets: [
+			"@babel/preset-typescript",
+			"@babel/preset-react",
+			"@babel/preset-env"
+		],
+		plugins: [
+			"@babel/plugin-proposal-class-properties"
+		]
+	}
+}
--- rollup.config.js
+++ rollup.config.js
@@ -27,6 +27,6 @@ module.exports = {
 		commonjs({
 			extensions
 		}),
-		// uglify()
+		uglify()
 	],
 };
